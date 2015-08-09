c2036410d553ab0fc6afa65d710c4fbd213ce846
Adam Thomson <ajt4@ualberta.ca>
2015-08-08 21:47:33 -0600

    Demo sprites, tried to fix pixel perfect
    Tile tearing is happening and it is making me a grumpy gus.

--- Adam and Matt Prototype/Assets/Standard Assets/2D/Scripts/OrthoCamera.cs
+++ Adam and Matt Prototype/Assets/Standard Assets/2D/Scripts/OrthoCamera.cs
@@ -7,11 +7,11 @@ public class OrthoCamera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Camera>().orthographicSize = (Screen.height / 10f / 2.0f) / scale;
+		GetComponent<Camera>().orthographicSize = (Screen.height / 1f / 2.0f) / (float)scale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Camera>().orthographicSize = (Screen.height / 10f / 2.0f) / scale;
+		GetComponent<Camera>().orthographicSize = (Screen.height / 1f / 2.0f) / (float)scale;
 	}
 }
--- Adam and Matt Prototype/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Adam and Matt Prototype/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -5,14 +5,14 @@ namespace UnityStandardAssets._2D
 {
     public class PlatformerCharacter2D : MonoBehaviour
     {
-        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+        //[SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
         [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        //[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-        const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+        const float k_GroundedRadius = .01f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
@@ -51,6 +51,7 @@ namespace UnityStandardAssets._2D
 
         public void Move(float move, bool crouch, bool jump)
         {
+			//Debug.Log("Move is: " + move);
             // If crouching, check to see if the character can stand up
             if (!crouch && m_Anim.GetBool("Crouch"))
             {
@@ -67,14 +68,20 @@ namespace UnityStandardAssets._2D
             //only control the player if grounded or airControl is turned on
             if (m_Grounded || m_AirControl)
             {
-                // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
+				Vector3 movement = transform.localPosition;
 
-                // The Speed animator parameter is set to the absolute value of the horizontal input.
-                m_Anim.SetFloat("Speed", Mathf.Abs(move));
+				// Move the character
+				if (move > 0)
+				{
+					movement.x += 1f;
+				}
+				else if (move < 0)
+				{
+					movement.x -= 1f;
+				}
 
-                // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+				transform.localPosition = movement;
+                //m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight)
